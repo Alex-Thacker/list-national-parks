96ec906dfba00bce6e18dcefa0a1553fee8dae00
Alex Thacker <thacker.alex@yahoo.com>
2019-03-18 15:01:52 -0500

    exercise done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,17 @@
-// Today’s exercise: Build an application using grunt to get data from the following API: https://github.com/nss-day-cohort-31/national-parks and display the list of national parks on the DOM. Each national park should be formatted in HTML like below:
-
-// If the park has been visited, the article tag should have a red dashed border. If the park has not been visited, it should have a green solid border.
-
-{
-    /* <article>
-          <h3>Park Name</h3>
-          <p>State the park in located in</p>
-        </article> */
-}
-
-
-
-// let article = document.createElement("article");
 let container = document.querySelector("#display-container")
-// let frag = document.createDocumentFragment()
-
-// const createElement = (parsed) => {
-//     let article = document.createElement("article");
-//     let h3 = document.createElement("h3");
-//     h3.textContent = parsed.name;
-//     let p = document.createElement("p");
-//     p.textContent = parsed.state;
-//     article.appendChild(h3);
-//     article.appendChild(p);
-//     frag.appendChild(article)
-// }
+let article = document.createElement("article");
+let frag = document.createDocumentFragment()
 
 const getPark = () => {
     return fetch("http://localhost:9099/parks")
         .then(response => response.json())
 }
+
+const getWeather = (latitude1, longitude1) => {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8774613f77c8add5e82f4f04ef1e87dc/${latitude1},${longitude1}`)
+        .then(response => response.json())
+}
+
 
 getPark()
     .then(parsedInt => {
@@ -40,8 +22,13 @@ getPark()
             h3.textContent = parsedInt[i].name;
             let p = document.createElement("p");
             p.textContent = parsedInt[i].state;
+            let p1 = document.createElement("p")
+            p1.textContent = "Weather: "
             article.appendChild(h3)
             article.appendChild(p)
+            article.appendChild(p1)
+            frag.appendChild(article)
+
             if (parsedInt[i].visited === true) {
 
                 article.classList.add("redBorder")
@@ -49,8 +36,28 @@ getPark()
             } else {
                 article.classList.add("greenBorder")
             }
-            container.append(article)
+
+            getWeather(parsedInt[i].latitude, parsedInt[i].longitude)
+                .then(response => {
+                    console.log(response)
+                    let ul = document.createElement("ul")
+                    let li1 = document.createElement("li")
+                    li1.textContent = "Currently: " + response.currently.summary
+                    ul.appendChild(li1);
+                    let li2 = document.createElement("li")
+                    li2.textContent = "Today: " + response.hourly.summary
+                    ul.appendChild(li2)
+                    let li3 = document.createElement("li")
+                    li3.textContent = "Week: " + response.daily.summary
+                    ul.appendChild(li3)
+                    article.appendChild(ul)
+                    frag.appendChild(article)
+
+
+                    container.appendChild(frag)
+                })
+
+
         }
-        
+
     })
-    
